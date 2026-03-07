4d16b595b51a1ec5b45ffcf7df4b6b688eb69053
Scott Roberts <scott.lee.roberts@gmail.com>
2026-03-07 14:03:44 -0500

    Modernize Ergodox EZ firmware to align with Iris keyboard
    - Consolidate common_keyboard.h and common_ergodox.h into single keymap.c
    - Replace deprecated M() macro system with SEND_STRING tmux macros
    - Update keycodes: RESET->QK_BOOT, KC__MUTE->KC_MUTE, KC__VOLUP->KC_VOLU,
      KC__VOLDOWN->KC_VOLD, LSFT(KC_LBRC)->KC_LCBR, CTL_T->LCTL_T
    - Add Chordal Hold, Flow Tap (100ms), Permissive Hold, Tapping Term (170ms)
    - Add combos: J+H=Esc, K+J=Backspace, D+F=Tab
    - Add chordal hold overrides for F+W (Ctrl+W) and L+Y (GUI+Y)
    - Replace individual RGB color keys with UG_* controls on Numbers layer
    - Add config.h and rules.mk (LTO, COMBO, NKRO=no)
    - Align Symbol layer with Iris: shifted symbols, arrows, ? \
    - Remove Ergodox LED indicator code (matrix_scan_user)
    - Remove Makefile, add Docker-based build instructions
    - Build target: ergodox_ez/shine:scottleeroberts
    - Firmware size: 23444/32256 (72%)

--- keymap.c
+++ keymap.c
@@ -1,181 +1,180 @@
 #include QMK_KEYBOARD_H
-#include "debug.h"
-#include "action_layer.h"
-#include "version.h"
-#include "common_keyboard.h"
-#include "common_ergodox.h"
+
+// Layer definitions
+enum layers {
+    BASE,
+    SYMB,
+    TMUX,
+    NUMBERS
+};
+
+// Custom keycodes for tmux macros and RGB mode selection
+enum custom_keycodes {
+    TMUX_WIN_1 = SAFE_RANGE,
+    TMUX_WIN_2,
+    TMUX_WIN_3,
+    TMUX_WIN_4,
+    TMUX_WIN_5,
+    TMUX_COPY_MODE,
+    TMUX_SPLIT_H,
+    TMUX_SPLIT_V,
+    TMUX_NEW_WIN,
+    RGB_PLAIN,
+    RGB_BREATHE
+};
+
+// Home-row modifiers
+#define MY_A LALT_T(KC_A)
+#define MY_SCLN LALT_T(KC_SCLN)
+#define MY_S LGUI_T(KC_S)
+#define MY_D LT(TMUX, KC_D)
+#define MY_F LCTL_T(KC_F)
+#define MY_G LSFT_T(KC_G)
+#define MY_H LSFT_T(KC_H)
+#define MY_J LCTL_T(KC_J)
+#define MY_L LGUI_T(KC_L)
+
+// Combos: press two adjacent keys simultaneously for common actions
+const uint16_t PROGMEM combo_jh[] = {MY_J, MY_H, COMBO_END};
+const uint16_t PROGMEM combo_kj[] = {KC_K, MY_J, COMBO_END};
+const uint16_t PROGMEM combo_df[] = {MY_D, MY_F, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(combo_jh, KC_ESC),    // J+H = Escape
+    COMBO(combo_kj, KC_BSPC),   // K+J = Backspace
+    COMBO(combo_df, KC_TAB),    // D+F = Tab
+};
+
+#ifdef RGBLIGHT_ENABLE
+void keyboard_post_init_user(void) {
+    rgblight_enable_noeeprom();
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap 0: Basic layer
-   *
-   * ,--------------------------------------------------.           ,--------------------------------------------------.
-   * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  Bkspc |
-   * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-   * | `      |   Q  |   W  |   E  |   R  |   T  |      |           |      |   Y  |   U  |   I  |   O  |   P  |   \    |
-   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-   * | CTRL   |   A  |cmd/S |tmux/D|  ^/F |   G  |------|           |------|   H  |  ^/J |  K   |cmd/L |  ;   |   "    |
-   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-   * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   |   =    |
-   * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-   *   |      |      |      | CMD |NumLayer|                                       |  L1 | TAB  |      |      |      |
-   *   `----------------------------------'                                       `----------------------------------'
-   *                                        ,-------------.       ,-------------.
-   *                                        |      |      |       |      |      |
-   *                                 ,------|------|------|       |------+--------+------.
-   *                                 |      |      |      |       |      |        |      |
-   *                                 | Enter|      |------|       |------|  Tab   |Space |
-   *                                 |      |      |      |       |      |        |      |
-   *                                 `--------------------'       `----------------------'
-   */
-  // If it accepts an argument (i.e, is a function), it doesn't need KC_.
-  // Otherwise, it needs KC_*
-  [BASE] = LAYOUT_ergodox(  // layer 0 : default
-      // left hand
-      KC_ESC  ,KC_1    ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,_______,
-      KC_GRAVE,KC_Q    ,KC_W   ,KC_E   ,KC_R   ,KC_T  ,_______,
-      KC_LCTRL,MY_A    ,MY_S   ,MY_D   ,MY_F   ,MY_G,
-      KC_LSFT ,KC_Z    ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,MY_V,
-      _______ ,_______   ,_______,KC_LCMD,MO(NUMBERS),
-      _______,_______,
-      _______,
-      KC_ENTER,_______ , _______,
-      // right hand
-      _______ ,KC_6    ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,
-      _______ ,KC_Y    ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_MINS,
-      MY_H    ,MY_J    ,KC_K   ,MY_L   ,MY_SCLN,KC_QUOT,
-      _______  ,KC_N    ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_EQUAL,
-      MO(SYMB),KC_TAB   ,_______  ,_______  ,_______,
-      _______ ,_______,
-      _______ ,
-      _______ ,KC_TAB  ,KC_SPC
-      ),
 
+    [BASE] = LAYOUT_ergodox(
+        // left hand
+        KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,
+        KC_GRAVE, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,
+        KC_LCTL,  MY_A,    MY_S,    MY_D,    MY_F,    MY_G,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,
+        _______,  _______, _______, KC_LCMD, MO(NUMBERS),
+                                                       _______, _______,
+                                                                _______,
+                                              KC_ENT,  _______, _______,
+        // right hand
+        _______,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        _______,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
+                  MY_H,    MY_J,    KC_K,    MY_L,    MY_SCLN, KC_QUOT,
+        _______,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQUAL,
+                           MO(SYMB),KC_TAB,  _______, _______, _______,
+        _______, _______,
+        _______,
+        _______, KC_TAB,  KC_SPC
+    ),
 
-  /* Keymap 1: Symbol Layer
-   *
-   * ,--------------------------------------------------.           ,--------------------------------------------------.
-   * | reset  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
-   * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-   * |  tab   |      |      |      |      |      |      |           |      |   |  |      |      |      |      |        |
-   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-   * |        |  [   |   ]  |   {  |   }  |      |------|           |------| left | down | up   | right|      |        |
-   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-   * |        |      |      |   (  |   )  |      |      |           |      |      | TEAL | RED  | GREEN| BLUE | YELLOW |
-   * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-   *   |TOGGLE|BREATH|KNIGHT| XMAS | OTHER|                                       |      |RAINBW| STILL| SWIRL|       |
-   *   `----------------------------------'                                       `----------------------------------'
-   *                                        ,-------------.       ,-------------.
-   *                                        |      |      |       |      |      |
-   *                                 ,------|------|------|       |------+------+------.
-   *                                 |      |      |      |       |      |      |      |
-   *                                 |      |      |------|       |------|      |      |
-   *                                 |      |      |      |       |      |      |      |
-   *                                 `--------------------'       `--------------------'
-   */
-  // SYMBOLS
-  [SYMB] = LAYOUT_ergodox(
-      // left hand
-      RESET  ,_______,_______,_______,_______,_______,_______,
-      KC_TILD, KC_EXLM , KC_AT,  KC_HASH, KC_DLR,  KC_PERC, _______,
-      _______,KC_LBRC,KC_RBRC,LSFT(KC_LBRC),LSFT(KC_RBRC),_______,
-      _______,_______,_______,KC_LEFT_PAREN,KC_RIGHT_PAREN,_______,_______,
-      TOGGLE ,BREATH ,KNIGHT ,XMAS ,OTHER,
-      _______,_______,
-      _______,
-      _______,_______,_______,
-      // right hand
-      _______   ,_______,_______,_______,_______,_______,_______,
-      _______   ,KC_CIRC,KC_AMPR,KC_ASTR,KC_LEFT_PAREN,KC_RIGHT_PAREN, KC_PIPE,
-      KC_LEFT   ,KC_DOWN,KC_UP ,KC_RIGHT,_______, KC_BSLS,
-      _______   ,_______,TEAL   ,RED     ,BLUE   ,GREEN   ,YELLOW,
-      _______   ,STILL  ,RAINBOW,SWIRL   ,_______,
-      _______   ,_______,
-      _______   ,
-      _______   ,_______,_______
-      ),
+    [SYMB] = LAYOUT_ergodox(
+        // left hand
+        _______,  _______, _______, _______, _______, _______, _______,
+        KC_TILD,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,
+        _______,  KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, _______,
+        _______,  _______, _______, KC_LPRN, KC_RPRN, _______, _______,
+        _______,  _______, _______, _______, KC_BSPC,
+                                                       _______, _______,
+                                                                _______,
+                                              _______, _______, _______,
+        // right hand
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+                  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_QUES, KC_BSLS,
+        _______,  _______, _______, _______, _______, _______, _______,
+                           _______, _______, _______, _______, _______,
+        _______, _______,
+        _______,
+        _______, _______, _______
+    ),
 
+    [TMUX] = LAYOUT_ergodox(
+        // left hand
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  _______, _______, _______, _______,
+                                                       _______, _______,
+                                                                _______,
+                                              _______, _______, _______,
+        // right hand
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  _______, TMUX_COPY_MODE, TMUX_SPLIT_H, TMUX_SPLIT_V, _______, _______,
+                  _______, TMUX_WIN_1, TMUX_WIN_2, TMUX_WIN_3, TMUX_WIN_4, TMUX_WIN_5,
+        _______,  TMUX_NEW_WIN, _______, _______, _______, _______, _______,
+                           _______, _______, _______, _______, _______,
+        _______, _______,
+        _______,
+        _______, _______, _______
+    ),
 
-      /* Keymap: Number Layer
-       *
-       * ,--------------------------------------------------.           ,--------------------------------------------------.
-       * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
-       * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |      |           |      |      |  7   |  8   |  9   |      |  MUTE  |
-       * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |------|           |------|      |  4   |   5  |  6   |      |  VolUp |
-       * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |      |           |      |      |  1   |   2  |  3   |      |  VolDn |
-       * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-       *   |      |      |      |      |      |                                       |  0   |      |      |      |      |
-       *   `----------------------------------'                                       `----------------------------------'
-       *                                        ,-------------.       ,-------------.
-       *                                        |      |      |       |      |      |
-       *                                 ,------|------|------|       |------+------+------.
-       *                                 |      |      |      |       |      |      |      |
-       *                                 |      |      |------|       |------|      |      |
-       *                                 |      |      |      |       |      |      |      |
-       *                                 `--------------------'       `--------------------'
-       */
-  // NUMBERS
-  [NUMBERS] = LAYOUT_ergodox(
-      // left hand
-      RESET  ,_______,_______,_______,_______,_______,_______,
-      KC_TAB ,_______,_______,_______,_______,_______,_______,
-      _______,_______,_______,_______,_______,_______,
-      _______,_______,_______,_______,_______,_______,_______,
-      _______,_______,_______,_______,_______,
-      _______,_______,
-      _______,
-      _______,_______,_______,
-      // right hand
-      _______,_______,_______,_______,_______,_______   ,_______,
-      _______,_______,KC_7   ,KC_8   ,KC_9   ,_______  ,KC__MUTE,
-      _______,KC_4   ,KC_5   ,KC_6   ,_______,KC__VOLUP,
-      _______,_______,KC_1   ,KC_2   ,KC_3   ,_______  ,KC__VOLDOWN,
-      KC_0   ,_______,_______,_______,_______,
-      _______,_______,
-      _______,
-      _______,_______,KC_TAB
-      ),
-      /* Keymap 3: Tmux layer
-       *
-       * ,--------------------------------------------------.           ,--------------------------------------------------.
-       * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
-       * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |      |           |      |      |cmode |  vs  |  sp  |      |        |
-       * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |------|           |------|      |  1   |   2  |  3   |  4   |   5    |
-       * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
-       * |        |      |      |      |      |      |      |           |      | new |      |      |      |      |        |
-       * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-       *   |      |      |      |      |      |                                       |      |      |      |      |      |
-       *   `----------------------------------'                                       `----------------------------------'
-       *                                        ,-------------.       ,-------------.
-       *                                        |      |      |       |      |      |
-       *                                 ,------|------|------|       |------+------+------.
-       *                                 |      |      |      |       |      |      |      |
-       *                                 |      |      |------|       |------|      |      |
-       *                                 |      |      |      |       |      |      |      |
-       *                                 `--------------------'       `--------------------'
-       */
-  [TMUX] = LAYOUT_ergodox(
-      //left hand
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,
-      _______, _______,
-      _______,
-      _______, _______, _______,
-      // right hand
-      _______, _______, _______, _______, _______, _______, _______,
-      _______, _______, M_TMUX_COPY_MODE, M_TMUX_SP, M_TMUX_VS, _______,_______,
-      _______, M_TMUX_1, M_TMUX_2, M_TMUX_3, M_TMUX_4, M_TMUX_5,
-      _______, M_TMUX_NEW, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, _______,
-      _______, _______,
-      _______,
-      _______, _______, _______
-      )
+    [NUMBERS] = LAYOUT_ergodox(
+        // left hand
+        QK_BOOT,  _______, _______, _______, _______, _______, _______,
+        _______,  RGB_PLAIN, RGB_BREATHE, _______, _______, _______, _______,
+        UG_TOGG,  UG_NEXT, UG_HUEU, UG_SPDU, UG_VALU, _______,
+        _______,  UG_PREV, UG_HUED, UG_SPDD, UG_VALD, _______, _______,
+        _______,  _______, _______, _______, _______,
+                                                       _______, _______,
+                                                                _______,
+                                              _______, _______, _______,
+        // right hand
+        _______,  _______, _______, _______, _______, _______, _______,
+        _______,  _______, KC_7,    KC_8,    KC_9,    _______, KC_MUTE,
+                  _______, KC_4,    KC_5,    KC_6,    _______, KC_VOLU,
+        _______,  _______, KC_1,    KC_2,    KC_3,    _______, KC_VOLD,
+                           KC_0,    _______, _______, _______, _______,
+        _______, _______,
+        _______,
+        _______, _______, KC_TAB
+    )
 };
+
+// Chordal hold override: allow specific same-hand shortcuts
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t *other_record) {
+    if (tap_hold_keycode == MY_F && other_keycode == KC_W) {
+        return true;
+    }
+    if (tap_hold_keycode == MY_L && other_keycode == KC_Y) {
+        return true;
+    }
+    return get_chordal_hold_default(tap_hold_record, other_record);
+}
+
+// Helper function: sends Ctrl+Space (tmux prefix) followed by a key
+void send_tmux_key(const char* key) {
+    SEND_STRING(SS_LCTL(" "));
+    send_string(key);
+}
+
+// Custom keycode processing
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!record->event.pressed) return true;
+
+    switch (keycode) {
+        case TMUX_WIN_1:      send_tmux_key("1"); return false;
+        case TMUX_WIN_2:      send_tmux_key("2"); return false;
+        case TMUX_WIN_3:      send_tmux_key("3"); return false;
+        case TMUX_WIN_4:      send_tmux_key("4"); return false;
+        case TMUX_WIN_5:      send_tmux_key("5"); return false;
+        case TMUX_COPY_MODE:  send_tmux_key("["); return false;
+        case TMUX_SPLIT_H:    send_tmux_key(SS_LSFT("'")); return false;
+        case TMUX_SPLIT_V:    send_tmux_key(SS_LSFT("5")); return false;
+        case TMUX_NEW_WIN:    send_tmux_key("c"); return false;
+#ifdef RGBLIGHT_ENABLE
+        case RGB_PLAIN:       rgblight_mode(RGBLIGHT_MODE_STATIC_LIGHT); return false;
+        case RGB_BREATHE:     rgblight_mode(RGBLIGHT_MODE_BREATHING); return false;
+#endif
+    }
+    return true;
+}
--- readme.md
+++ readme.md
@@ -1,15 +1,178 @@
-## Building
+# Scott's Ergodox EZ Layout
 
-To build it you will need [qmk_firmware][qmk] checked out and this repo checked
-out to something like `keyboards/ergodox/keymaps/scottleeroberts`.
+Custom QMK firmware for Ergodox EZ Shine with home-row modifiers and tmux integration.
 
- [qmk]: https://github.com/jackhumbert/qmk_firmware
+## Features
+
+- **Home-row modifiers**: Alt, Cmd, Ctrl, Shift on home row (ASDF / JKL;)
+- **4 layers**: Base (QWERTY), Symbols, Tmux shortcuts, Numbers/RGB
+- **Tmux integration**: Custom macros for window management (Ctrl+Space prefix)
+- **RGB lighting**: Full control via Numbers layer
+- **Home-row combos**: J+H = Escape, K+J = Backspace, D+F = Tab
+- **Chordal Hold + Flow Tap**: Anti-misfire for home-row mods
+- **Modern QMK**: Updated for latest QMK firmware (Mar 2026)
+
+## Building and Flashing
+
+### Prerequisites
+
+```bash
+# Install Docker
+sudo apt install docker.io
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+# Log out and back in for group to take effect
+
+# Install Teensy loader (for flashing)
+sudo apt install teensy-loader-cli
+```
+
+### Build
+
+```bash
+cd ~/scottleeroberts/ergodox
+mkdir -p build && cd build
+
+# Clone QMK (first time only)
+git clone https://github.com/qmk/qmk_firmware.git
+cd qmk_firmware
+git submodule update --init --recursive
+
+# Copy keymap files
+mkdir -p keyboards/ergodox_ez/keymaps/scottleeroberts
+cp ../../keymap.c keyboards/ergodox_ez/keymaps/scottleeroberts/
+cp ../../config.h keyboards/ergodox_ez/keymaps/scottleeroberts/
+cp ../../rules.mk keyboards/ergodox_ez/keymaps/scottleeroberts/
+
+# Build firmware (use ergodox_ez/shine for Shine variant, ergodox_ez/base for Base)
+docker run --rm -v $(pwd):/qmk_firmware -w /qmk_firmware ghcr.io/qmk/qmk_cli \
+  make ergodox_ez/shine:scottleeroberts
+
+# Copy hex out
+cp ergodox_ez_shine_scottleeroberts.hex ../..
+cd ../..
+```
+
+### Flash
+
+The Ergodox EZ uses the Teensy/Halfkay bootloader. Press the small RESET button on the back of the keyboard (or hit QK_BOOT on the Numbers layer), then:
+
+```bash
+teensy_loader_cli -v -mmcu=atmega32u4 -w ergodox_ez_shine_scottleeroberts.hex
+```
+
+Or use QMK's built-in flash:
+```bash
+# From build/qmk_firmware directory
+make ergodox_ez/shine:scottleeroberts:flash
+```
+
+## Layer Guide
+
+### BASE (Layer 0) - Default QWERTY
 
 ```
-$ `git clone https://github.com/jackhumbert/qmk_firmware.git`
-$ `cd qmk_firmware`
-$ `git checkout 212aeee202`
-$ `git clone https://github.com/scottleeroberts/ergodox.git \keyboards/ergodox_ez/keymaps/scottleeroberts`
-$ from the root of qmk_firware, run `./util/docker_build.sh ergodox_ez:scottleeroberts`
-$ teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_scottleeroberts.hex
+,--------------------------------------------------.           ,--------------------------------------------------.
+| ESC  |  1  |  2  |  3  |  4  |  5  |     |           |     |  6  |  7  |  8  |  9  |  0  | BkSp |
+|------+-----+-----+-----+-----+-----------|           |-----+-----+-----+-----+-----+-----+------|
+|  `   |  Q  |  W  |  E  |  R  |  T  |     |           |     |  Y  |  U  |  I  |  O  |  P  |  -   |
+|------+-----+-----+-----+-----+-----|     |           |     |-----+-----+-----+-----+-----+------|
+| Ctrl |  A  |  S  |  D  |  F  |  G  |-----|           |-----|  H  |  J  |  K  |  L  |  ;  |  '   |
+|      | Alt | Cmd | Tmx | Ctl | Sft |     |           |     | Sft | Ctl |     | Cmd | Alt |      |
+|------+-----+-----+-----+-----+-----|     |           |     |-----+-----+-----+-----+-----+------|
+| Sft  |  Z  |  X  |  C  |  V  |  B  |     |           |     |  N  |  M  |  ,  |  .  |  /  |  =   |
+`------+-----+-----+-----+-----+-----------'           `-----------+-----+-----+-----+-----+------'
+  |    |     |     | Cmd | Num |                                   | Sym | Tab |     |     |     |
+  `--------------------------------'                               `--------------------------------'
+                                     ,-----------.       ,-----------.
+                                     |     |     |       |     |     |
+                              ,------+-----+-----|       |-----+-----+------.
+                              |      |     |     |       |     |     |      |
+                              | Entr |     |-----|       |-----|  Tab| Space|
+                              |      |     |     |       |     |     |      |
+                              `------------------'       `------------------'
+```
 
+### SYMB (Layer 1) - Symbols & Navigation
+
+- **Top row:** `~ ! @ # $ % ^ & * ( ) |`
+- **Home row left:** `[ ] { }`
+- **Home row right:** Arrow keys + `? \`
+- **Bottom row:** `( )`
+
+### TMUX (Layer 2) - Terminal Multiplexer
+
+Activated by holding D key. All macros send Ctrl+Space prefix:
+
+| Key | Action | Sends |
+|-----|--------|-------|
+| U | Copy mode | Ctrl+Space [ |
+| I | Split horizontal | Ctrl+Space " |
+| O | Split vertical | Ctrl+Space % |
+| J | Window 1 | Ctrl+Space 1 |
+| K | Window 2 | Ctrl+Space 2 |
+| L | Window 3 | Ctrl+Space 3 |
+| ; | Window 4 | Ctrl+Space 4 |
+| ' | Window 5 | Ctrl+Space 5 |
+| N | New window | Ctrl+Space c |
+
+### NUMBERS (Layer 3) - Numpad & RGB
+
+**Right side - Number pad:**
+```
+    7  8  9    Mute
+    4  5  6    Vol+
+    1  2  3    Vol-
+ 0
+```
+
+**Left side - RGB controls:**
+- **Row 2:** Plain mode (static), Breathing mode
+- **Row 3:** Toggle, Next mode, Hue+, Speed+, Brightness+
+- **Row 4:** Prev mode, Hue-, Speed-, Brightness-
+
+**Top left:** Reset/Bootloader (QK_BOOT)
+
+## Configuration Details
+
+### Home-Row Modifiers
+
+- **Tapping term:** 170ms
+- **Chordal Hold:** Same-hand tap-hold + next key resolves as tap (prevents misfires)
+- **Flow Tap:** Keys pressed within 100ms of each other always tap (fast typing protection)
+- **Permissive Hold:** Mod activates immediately when another key is tapped during hold
+
+### Combos
+
+| Combo | Action |
+|-------|--------|
+| J + H | Escape |
+| K + J | Backspace |
+| D + F | Tab |
+
+### Chordal Hold Exceptions
+
+| Combo | Action |
+|-------|--------|
+| F + W | Ctrl+W (same-hand, allowed) |
+| L + Y | GUI+Y (same-hand, allowed) |
+
+## Files
+
+| File | Purpose |
+|------|---------|
+| `keymap.c` | Main keymap and macro definitions |
+| `config.h` | Keyboard configuration settings |
+| `rules.mk` | Build rules and feature enables |
+
+## Variants
+
+| Build Target | Model |
+|-------------|-------|
+| `ergodox_ez/shine` | Ergodox EZ Shine (RGB underglow) |
+| `ergodox_ez/base` | Ergodox EZ Base (no RGB) |
+| `ergodox_ez/glow` | Ergodox EZ Glow (per-key RGB) |
+
+## License
+
+GPL v2 (inherited from QMK Firmware)
